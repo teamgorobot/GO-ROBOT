ee96e11b8844b3ef7bce3309c2e03f10141247c6
GO ROBOT <teamgorobot@gmail.com>
2026-09-14 10:52:44 +0200

    Update basecode.py

--- basecode.py
+++ basecode.py
@@ -97,6 +97,9 @@ class speed:
     @staticmethod
     def slow():
         drivebase.settings(200, 200, 100, 100)
+    def set(speed=200)
+        speed2 = speed / 2
+        drivebase.settings(speed, speed, speed2, speed2)
 if debug >= 2:
     print("Motor settings set successfully")
 
